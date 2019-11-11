adabf30f4b12416b66426f4f1dd6f5845a7914c3
rebornkumar <iitr.anujkumar@gmail.com>
2019-11-12 02:34:49 +0530

    formatting

--- DCP-1.cpp
+++ DCP-1.cpp
@@ -1,44 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool helper(vector<int>&A,int K)
-{
+bool helper(vector<int>&A,int K){
 	int N=A.size();
 	unordered_map<int,int>hash;
-	for(int i=0;i<N;i++)
-	{
-		if(hash.find(A[i]) == hash.end())
-		{
+	for(int i=0;i<N;i++){
+		if(hash.find(A[i]) == hash.end()){
 			hash[A[i]] = 1;
 		}
-		else
-		{
+		else{
 			hash[A[i]]++;
 		}
 	}
 	
-	for(int i=0;i<N;i++)
-	{
-		if(A[i]*2 == K && hash[A[i]] >=2)
-		{
+	for(int i=0;i<N;i++){
+		if(A[i]*2 == K && hash[A[i]] >=2){
 			
 			return true;
 		}
-		else if(hash.find(K-A[i]) != hash.end())
-		{
+		else if(hash.find(K-A[i]) != hash.end()){
 			return true;
 		}
 	}
 	return false;
 }
-int main() 
-{
+
+int main() {
 	//code
 	int N;
 	cin>>N;
 	vector<int>A(N,0);
-	for(int i=0;i<N;i++)
-	{
+	for(int i=0;i<N;i++){
 		cin>>A[i];
 	}
 	int K;
