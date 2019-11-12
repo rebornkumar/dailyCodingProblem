415c2e18f40ab9f07c6a2892706af760f0e026f8
rebornkumar <iitr.anujkumar@gmail.com>
2019-11-12 23:29:52 +0530

    missing part

--- DCP-104.cpp
+++ DCP-104.cpp
@@ -9,6 +9,43 @@ struct DoublyListNode {
 	DoublyListNode(int x) : val(x), next(NULL), prev(NULL) {}
 };
 
+DoublyListNode* createDoublyListNode(DoublyListNode* head,DoublyListNode* curr,vector<int>&A,int index) {
+
+	if(index == A.size())
+	{
+		return head;
+	}
+	if(index == 0) {
+		head = new DoublyListNode(A[index]);
+		DoublyListNode*  curr = head;
+		return createDoublyListNode(head,curr,A,index+1);
+	}
+	DoublyListNode*  doublyListNode= new DoublyListNode(A[index]);
+
+	curr -> next = doublyListNode;
+	doublyListNode->prev = curr;
+	curr = doublyListNode;
+	return createDoublyListNode(head,curr,A,index+1);
+}
+bool isPalindrome(DoublyListNode* head) {
+	if(head == NULL) return true;
+
+	DoublyListNode* tail = head;
+	while(tail->next) {
+		tail = tail->next;
+	}
+
+	while(head) {
+		if(head -> val != tail -> val) {
+			return false;
+		}
+		else {
+			head = head->next;
+			tail = tail -> prev;
+		}
+	}
+	return true;
+}
 
 int main() {
 	int N;
@@ -19,7 +56,8 @@ int main() {
 		cin>>A[i];
 	}
 
-	DoublyListNode* head = createDDoublyListNode(A,N);
+	int index = 0;
+	DoublyListNode* head = createDoublyListNode(NULL,NULL,A,index);
 	bool flag = isPalindrome(head);
 	if(flag) {
 		cout<<"LIST IS PALINDROME"<<endl;
