b3af3f52f4db353944a8dca5a572d77d3e0a824b
rebornkumar <iitr.anujkumar@gmail.com>
2019-11-30 15:18:06 +0530

    DCP-118,easy

---
+++ DCP-118.cpp
@@ -0,0 +1,61 @@
+/**
+This problem was asked by Google.
+Given a sorted list of integers, square the elements and give the output in sorted order.
+For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	//code
+	int N;
+	cin>>N;
+	vector<int>A(N,0);
+	for(int i=0;i<N;i++)
+	{
+		cin>>A[i];
+	}
+	int ptrNeg = -1,ptrPos = N;
+	//set ptrNeg,
+	for(int i=0;i<N;i++)
+	{
+		if(A[i] < 0)
+		{
+			ptrNeg = i;
+		}
+	}
+	if(ptrNeg>=0)
+	{
+		ptrPos = ptrNeg+1;
+	}
+	vector<int>ans(N,0);
+	int index = 0;
+	while(ptrNeg >= 0 && ptrPos < N)
+	{
+		if(abs(A[ptrNeg]) < abs(A[ptrPos]))
+		{
+			ans[index++] = pow(A[ptrNeg--],2);
+		}
+		else
+		{
+			ans[index++] = pow(A[ptrPos++],2);
+		}
+	}
+	while(ptrNeg >= 0)
+	{
+		ans[index++] = pow(A[ptrNeg--],2);
+	}
+	while(ptrPos < N)
+	{
+		ans[index++] = pow(A[ptrPos++],2);
+	}
+	for(int i=0;i<N;i++)
+	{
+		cout<<ans[i]<<" ";
+	}
+	cout<<""<<endl;
+	//code
+return 0;
+}
